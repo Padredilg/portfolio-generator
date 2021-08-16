92638134bd01b7634942dfffaf53152c71fe8c21
Luiz P <padredilg@knights.ucf.edu>
2021-08-16 13:57:18 -0400

    used npm install to install inquirer and created first prompt

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-//we want to use fs methods
-const fs = require('fs');
-//module.exports assignment in the page-template file will be reassigned to this generate page const
-const generatePage = require('./src/page-template.js');
+// //we want to use fs methods
+// const fs = require('fs');
+const inquirer = require('inquirer');
+// //module.exports assignment in the page-template file will be reassigned to this generate page const
+// const generatePage = require('./src/page-template.js');
 
-//two inputs are being expected starting at index 2 --> node app ____ ____
-const profileDataArgs = process.argv.slice(2);
+// //use the inputs in the template literal and write that to index.html
+// fs.writeFile('./index.html', generatePage(name, github), err => {//stops if error, otherwise notifies success in terminal
+//   if (err) throw new Error(err);
 
-//get the two inputs
-const [name, github] = profileDataArgs;
-
-//use the inputs in the template literal and write that to index.html
-fs.writeFile('./index.html', generatePage(name, github), err => {//stops if error, otherwise notifies success in terminal
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
