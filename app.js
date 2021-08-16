4825c5e2871e0c64118471052348e832cb09d82c
Luiz P <padredilg@knights.ucf.edu>
2021-08-16 11:45:49 -0400

    required fs and modularized template literal

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-//This function creates an array with whatever is written after node app.js
-//slice returns a copied array with elements from the (start, to end-1);
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+//we want to use fs methods
+const fs = require('fs');
+//module.exports assignment in the page-template file will be reassigned to this generate page const
+const generatePage = require('./src/page-template.js');
 
-//this is an arrow function. profileDataArr has no parentheses because its the only parameter. 
-//Function will be executed on a call.
-const printProfileData = profileDataArr => {
-    //whatever the code after the arrow is, it will be executed for each element in the profileDataArr
-    //if the function after the arrow is a single line, it does not need {}
-    //The element at the iteration is passed to profileItem. 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-}
+//two inputs are being expected starting at index 2 --> node app ____ ____
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+//get the two inputs
+const [name, github] = profileDataArgs;
+
+//use the inputs in the template literal and write that to index.html
+fs.writeFile('./index.html', generatePage(name, github), err => {//stops if error, otherwise notifies success in terminal
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
