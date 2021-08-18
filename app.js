403eed8b9310629fb53074e254d8d02dd790b250
Luiz P <padredilg@knights.ucf.edu>
2021-08-17 23:25:48 -0400

    included questions for projects, and added feature for new projects to be added

--- app.js
+++ app.js
@@ -10,12 +10,96 @@ const inquirer = require('inquirer');
 
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
-inquirer
-  .prompt([
+
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
       message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your Github Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
     }
-  ])
-  .then(answers => console.log(answers));
+  ]);
+};
+
+const promptProject = portfolioData => {
+
+  if(!portfolioData.projects){
+    portfolioData.projects =[];
+  }
+
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ]).then(projectData => {
+    portfolioData.projects.push(projectData);
+    
+    if(projectData.confirmAddProject){
+      return promptProject(portfolioData);
+    }
+    else{
+      return portfolioData
+    }
+  });
+};
+
+//so when promptProjects is called, then the user fill in info for first project.
+//the info is pushed in to the portfolioData.projects.
+//then, if confirmAddProjects is true, the function is called again, 
+//and so we dont leave the initial return inquirer.prompt yet, instead, we go inside another rabbit hole that will
+//keep pushing/append new info to portfolioData.projects until confirmAddProject is false.
+//when false, all rabbit holes will be finished and we will finally be returned from inquirer.prompt and head
+//to next step, where the object is passed to projectAnswers.
+
+//whenever we give then a param, the param receives an object from the function that it is attached to
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData)
+  });
